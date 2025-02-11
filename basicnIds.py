5ea4b2a201903c194389374ed3dd2210eb1b6650
Atakan Erdoğan <atakanerdognn@outlook.com>
2025-02-11 22:28:41 +0300

    Update basicnIds.py
    Comment lines updated

--- basicnIds.py
+++ basicnIds.py
@@ -3,14 +3,14 @@ import struct
 import os
 
 # Dosya kontrolü ve okunması
-if not os.path.exists('scrapy_iplist.txt'): # Zararlı ip adreslerinin bulunduğu dosya. Bu dosya dinamik olarak web scraping  ile sürekli güncellenecek şekilde ayarlanabilir.     print("Error: 'scrapy_iplist.txt' file not found.")
+if not os.path.exists('scrapy_iplist.txt'): # Zararlı ip adreslerinin bulunduğu dosya. Bu dosya dinamik olarak web scraping  ile sürekli güncellenecek şekilde ayarlanabilir.    
     exit()
 
 with open('scrapy_iplist.txt', 'r') as file:
     monitored_ips = [line.strip() for line in file.readlines()]
 
 try:
-    host = socket.gethostbyname(socket.gethostname()) #Bu kod, bilgisayarın ağ paketlerini doğrudan dinlemeye ve manipüle etmeye yarayan bir ham soket oluşturur. 
+    host = socket.gethostbyname(socket.gethostname()) #Bu kod, bilgisayarın ağ paketlerini doğrudan dinlemeye ve manipüle etmeye yarayan bir ham soket oluştururr. 
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)  # IPPROTO_TCP daha yaygın
     s.bind((host, 0))
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)  # Header dahil etmek için set işlemi yapılır.
@@ -31,11 +31,11 @@ while True:
     ihl = version_ihl & 0xF
     iph_length = ihl * 4  
 
-    src_addr = socket.inet_ntoa(iph[8])  # socket.inet_ntoa() fonksiyonu, binary (ikili) formatta bir IP adresini, insan tarafından okunabilir bir IPv4 adresi formatına dönüştüren bir Python fonksiyonudur. 4s listedeki 8 ve 9. elemanlara denk gelir.
+    src_addr = socket.inet_ntoa(iph[8])  # socket.inet_ntoa() fonksiyonu, binary (ikili) formatta bir IP adresini, insan tarafından okunabilir bir IPv4 adresi formatına dönüştüren bir Python fonksiyonudur. 4s listedeki 8 ve 9. elemanlara yani ip adreslerine denk gelir.
     dst_addr = socket.inet_ntoa(iph[9])
 
-    if src_addr in monitored_ips:
-        print(f"Alert: Incoming connection for blacklisted IP {src_addr}!")
+    if src_addr in monitored_ips: # Zararlı trafikler yakalanmış oldu burda.
+        print(f"Alert: Incoming connection for blacklisted IP {src_addr}!") 
 
     if dst_addr in monitored_ips:
         print(f"Alert: Outgoing connection for blacklisted IP {dst_addr}!")
